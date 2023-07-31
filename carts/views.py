9ca249c79159f7606964e63cd09be8c0884317c8
ProgramerSalar <manishkumar60708090@gmail.com>
2023-07-31 14:03:12 +0530

    create the item in seperate wise

--- carts/views.py
+++ carts/views.py
@@ -54,12 +54,12 @@ def add_cart(request, product_id):
         cart.save()
 
     try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item = CartItem.objects.create(product=product, quantity = 1, cart=cart)
         if len(product_variation) > 0:
             cart_item.variations.clear()
             for item in product_variation:
                 cart_item.variations.add(item)
-        cart_item.quantity += 1 
+        # cart_item.quantity += 1 
         cart_item.save()
 
     except CartItem.DoesNotExist:
